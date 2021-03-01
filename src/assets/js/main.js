9a4f0a3145dd050b7619bb68934986e767eb5a0c
Mithila Malinda <mithilam@99x.io>
2021-03-02 00:01:00 +0530

    New section added

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -23,4 +23,39 @@ $(function () {
         cssEase: 'linear',
         Infinite: true
     });
+    //slick slider ends
+
+    //testimonial-slider starts
+    $('.testimonial-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 500,
+        autoplaySpeed: 8000,
+        arrows: false,
+        dots: true,
+        cssEase: 'linear',
+        Infinite: true
+    });
+    //testimonial-slider ends
+
+    //fixed top bar class toggle starts
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 56) {
+            $(".navbar").addClass("navbar-sticky-top");
+        } else {
+            $(".navbar").removeClass("navbar-sticky-top");
+        }
+    });
+    $(".navbar-toggler").click(function () {
+        if (!$(".navbar-collapse").hasClass("show")) {
+            $(".navbar").addClass("navbar-sticky-top");
+        } else {
+            if ($(window).scrollTop() < 56) {
+                $(".navbar").removeClass("navbar-sticky-top");
+            } else {
+            }
+        }
+    });
+    //fixed top bar class toggle ends
 });
